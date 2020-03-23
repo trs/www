c69e29608f263521ebdbf966596e51c4979c26a6
Tyler Stewart <hello@tylerstewart.ca>
2020-03-23 17:15:00 -0600

    bye tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -8,7 +8,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 module.exports = {
   module: false,
   plugins: [
-    require('tailwindcss'),
     require('autoprefixer'),
     ...process.env.NODE_ENV === 'production'
       ? [purgecss]
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  theme: {
-    fontFamily: {
-      default: 'Inter, sans-serif',
-      display: 'Inter, sans-serif',
-      body: 'Inter, sans-serif'
-    }
-  }
-}
