c6f3ff5826e1170352c7c2b45fdfd53d37eec7b1
Tyler Stewart <hello@tylerstewart.ca>
2021-12-12 22:31:39 -0700

    redesign

---
+++ src/lib/components/avatar.svelte
@@ -0,0 +1,3 @@
+<div class="rounded-full border-8 border-sky-400 dark:border-sky-500">
+  <img src="/tyler.png" width="160px" alt="Tyler Stewart" draggable={false} class="select-none brightness-[90%] rounded-full border-8 border-slate-50 dark:border-slate-800 bg-sky-400 dark:bg-sky-500">
+</div>
