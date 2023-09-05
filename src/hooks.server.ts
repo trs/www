25a137c52fed1f9a4ac65a1e4eca3968c08dcaef
Tyler Stewart <hello@tylerstewart.ca>
2023-09-04 18:08:41 -0600

    fix hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,8 @@
 import type {Handle} from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const theme = event.cookies.get("theme");
-  const motion = event.cookies.get("motion");
+  const theme = event.cookies.get("theme") ?? "";
+  const motion = event.cookies.get("motion") ?? "";
 
   const response = await resolve(event, {
     transformPageChunk: ({ html }) => {
